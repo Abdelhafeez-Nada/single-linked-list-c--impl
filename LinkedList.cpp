a9d08f992e457c8a3d2402b9540179ffd22a18d2
AbdelhafeezNada <abdelhafeez0nada@gmail.com>
2024-09-03 13:40:58 +0300

    implement insert_end

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -15,6 +15,29 @@ private:
     Node *tail{};
 
 public:
-    void print() {}
-    void insert_end(int val) {}
+    void print()
+    {
+        Node *current = head;
+        while (current != nullptr)
+        {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+
+    void insert_end(int val)
+    {
+        Node *newNode = new Node(val);
+        Node *current = head;
+        if (head == nullptr)
+        {
+            tail = newNode;
+            head = newNode;
+        }
+        while (current->next != nullptr)
+            current = current->next;
+        current->next = newNode;
+        tail = newNode;
+    }
 };

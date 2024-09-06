a16a9655f6de2d2bf5d8d1f9dfeeb9a1319c88df
AbdelhafeezNada <abdelhafeez0nada@gmail.com>
2024-09-06 19:56:57 +0300

    create LinkedList_head_only

---
+++ LinkedList_head_only.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Node.cpp";
+using namespace std;
+
+class LinkedList_head_only
+{
+private:
+    Node *head{};
+
+public:
+    ~LinkedList_head_only();
+};
+
+LinkedList_head_only::LinkedList_head_only(/* args */)
+{
+}
+
+LinkedList_head_only::~LinkedList_head_only()
+{
+}

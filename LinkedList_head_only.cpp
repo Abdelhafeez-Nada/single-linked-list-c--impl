3126817753a3a7032a25f34c7aa6885cc4f84527
AbdelhafeezNada <abdelhafeez0nada@gmail.com>
2024-09-06 20:16:31 +0300

    implement destructor,add_element & print

--- LinkedList_head_only.cpp
+++ LinkedList_head_only.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-#include "Node.cpp";
 using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int data) : data(data) {}
+};
 
 class LinkedList_head_only
 {
@@ -8,13 +14,40 @@ private:
     Node *head{};
 
 public:
-    ~LinkedList_head_only();
+    ~LinkedList_head_only()
+    {
+        cout << "\n LinkedList_head_only Destructor Started !";
+        while (head != nullptr)
+        {
+            Node *oldHead = head;
+            head = head->next;
+            delete oldHead;
+        }
+    }
+
+    void add_element(int value)
+    {
+        Node *newNode = new Node(value);
+        if (head == nullptr)
+        {
+            head = newNode;
+            return;
+        }
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void print()
+    {
+        for (Node *cur = head; cur; cur = cur->next)
+            cout << cur->data << " ";
+    }
+
+    Node *get_tail()
+    {
+        Node *current = head;
+        while (current->next != nullptr)
+            current = current->next;
+        return current;
+    }
 };
-
-LinkedList_head_only::LinkedList_head_only(/* args */)
-{
-}
-
-LinkedList_head_only::~LinkedList_head_only()
-{
-}
--- main.cpp
+++ main.cpp
@@ -1,36 +1,52 @@
 #include <iostream>
-#include "LinkedList.cpp"
+// #include "LinkedList.cpp"
+#include "LinkedList_head_only.cpp"
 using namespace std;
 
 int main()
 {
-    LinkedList list;
 
-    list.insert_end(11);
+    LinkedList_head_only list;
 
-    list.insert_end(2);
+    list.add_element(1);
+    list.add_element(2);
+    list.add_element(3);
+    list.add_element(4);
+    list.add_element(5);
+    list.add_element(6);
 
-    list.insert_end(3);
+    list.print();
 
-    list.insert_end(41);
+    cout << endl;
+    cout << list.get_tail()->data;
 
-    list.insert_end(5);
+    // LinkedList list;
 
-    list.insert_end(6);
+    // list.insert_end(11);
 
-    LinkedList list2;
+    // list.insert_end(2);
 
-    list2.insert_end(11);
+    // list.insert_end(3);
 
-    list2.insert_end(2);
+    // list.insert_end(41);
 
-    list2.insert_end(3);
+    // list.insert_end(5);
 
-    list2.insert_end(41);
+    // list.insert_end(6);
 
-    list2.insert_end(5);
+    // LinkedList list2;
 
-    // list2.insert_end(6);
+    // list2.insert_end(11);
 
-    cout << list.is_same_use_length(list2) << endl;
+    // list2.insert_end(2);
+
+    // list2.insert_end(3);
+
+    // list2.insert_end(41);
+
+    // list2.insert_end(5);
+
+    // // list2.insert_end(6);
+
+    // cout << list.is_same_use_length(list2) << endl;
 }
